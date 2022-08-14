27d93369f0fb2f12b6c920558af555a4b4bd3d29
Bsaltori <biasaltori@yahoo.com.br>
2022-08-14 18:36:22 -0300

    Primeiras aulas

---
+++ ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+result = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, result))
---
+++ ex004.py
@@ -0,0 +1,13 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ', a.isalnum())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico?', a.isalnum())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minúsculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
+
+
+
+
